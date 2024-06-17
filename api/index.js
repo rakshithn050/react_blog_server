3ec8876abe0991d8113e07309c600e91bdfb9c94
rakshithn050 <rakshith14022001@gmail.com>
2024-06-17 17:40:21 +0530

    Database Connection Established

--- api/index.js
+++ api/index.js
@@ -1,9 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const result = dotenv.config();
+
+mongoose
+  .connect(process.env.MongoURL)
+  .then(() => {
+    console.log("mongodb connection successful");
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+  console.log("listening on port 3000");
 });
