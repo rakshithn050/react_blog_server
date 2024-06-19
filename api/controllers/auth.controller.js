7ced7afb4910bf8175989583e67454eacbe0348f
rakshithn050 <rakshith14022001@gmail.com>
2024-06-19 15:39:06 +0530

    Login Functionalities Created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,9 +10,9 @@ export const signup = async (req, res, next) => {
     !username ||
     !email ||
     !password ||
-    username.length === 0 ||
-    email.length === 0 ||
-    password.length === 0
+    username.trim().length === 0 ||
+    email.trim().length === 0 ||
+    password.trim().length === 0
   ) {
     next(errorHandler(400, "All Fields are required"));
   }
@@ -25,6 +26,51 @@ export const signup = async (req, res, next) => {
 
     res.json({ message: "Signup Successful" });
   } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (
+    !email ||
+    !password ||
+    email.trim().length === 0 ||
+    password.trim().length === 0
+  ) {
+    next(errorHandler(400, "All Fields are required"));
+  }
+
+  try {
+    // Find user by email
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      next(errorHandler(404, "User not Found"));
+    }
+
+    // Check if password matches
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      next(errorHandler(401, "Incorrect password"));
+    }
+
+    const { password: pass, ...restUserInfo } = validUser._doc;
+
+    // Generate JWT token
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.SECRET_KEY
+      // { expiresIn: "1h", } // Example: token expires in 1 hour
+    );
+
+    // Set token as a cookie (assuming you are using cookies)
+    res.cookie("access_token", token, { httpOnly: true });
+
+    // Respond with success message and token (you may customize the response as needed)
+    res.status(200).json({ message: "Signin successful", restUserInfo, token });
+  } catch (error) {
+    // Forward error to the next error-handling middleware
     next(error);
   }
 };
