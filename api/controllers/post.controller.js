309bedc4204c5663f3b5b002ae02eac44617e3ce
rakshithn050 <rakshith14022001@gmail.com>
2024-06-30 13:21:33 +0530

    Extended the functionality of create Post

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,6 +1,27 @@
+import Post from "../models/post.model.js";
+import { errorHandler } from "../utils/error.js";
+
 export const createNewPost = async (req, res, next) => {
   try {
+    if (!req.user.isAdmin) {
+      return next(errorHandler(403, "You are not allowed to create a post"));
+    }
+    if (!req.body.title || !req.body.description) {
+      return next(errorHandler(400, "Please fill all the required fields"));
+    }
+
+    const slug = req.body.title
+      .split(" ")
+      .join("-")
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9-]/g, "-");
+
+    const newPost = new Post({ ...req.body, slug, userID: req.user.id });
+
+    const savedPost = await newPost.save();
+
+    return res.status(201).json(savedPost);
   } catch (error) {
-    next(error);
+    return next(error);
   }
 };
--- api/models/post.model.js
+++ api/models/post.model.js
@@ -2,22 +2,37 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
   {
-    title: {
-      type: "string",
+    userID: {
+      type: String,
       required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
     },
     category: {
-      type: "string",
-      required: true,
+      type: String,
+      default: "general",
     },
     description: {
-      type: "text",
+      type: String,
       required: true,
+    },
+    image: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1499750310107-5fef28a66643?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmxvZ3xlbnwwfHwwfHx8MA%3D%3D",
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
     },
   },
   { timestamps: true }
 );
 
-const Post = new mongoose.Model("post", PostSchema);
+const Post = mongoose.model("Post", PostSchema);
 
 export default Post;
