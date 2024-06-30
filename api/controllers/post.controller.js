ba322f926c9d6d255d414cd4bdda8fd2dc33202e
rakshithn050 <rakshith14022001@gmail.com>
2024-06-30 17:38:52 +0530

    getPosts with query handling api created

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -16,6 +16,8 @@ export const createNewPost = async (req, res, next) => {
       .toLowerCase()
       .replace(/[^a-zA-Z0-9-]/g, "-");
 
+    console.log(slug);
+
     const newPost = new Post({ ...req.body, slug, userID: req.user.id });
 
     const savedPost = await newPost.save();
@@ -23,5 +25,51 @@ export const createNewPost = async (req, res, next) => {
     return res.status(201).json(savedPost);
   } catch (error) {
     return next(error);
+  }
+};
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 0;
+    const sortBy = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find({
+      ...(req.query.userID && { userID: req.query.userID }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+        ],
+      }),
+    })
+      .sort({ updatedAt: sortBy })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalPosts = await Post.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+
+    const lastMonthPosts = await Post.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      posts,
+      totalPosts,
+      lastMonthPosts,
+    });
+  } catch (error) {
+    next(error);
   }
 };
--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createNewPost } from "../controllers/post.controller.js";
+import { createNewPost, getPosts } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/authVerify.js";
 
 const router = express.Router();
 
 router.post("/create-post", verifyToken, createNewPost);
+router.get("/getPosts", getPosts);
 
 export default router;
