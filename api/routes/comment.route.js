bb7c4645c9a6cb41a1ac223c126c9f00247bda5b
rakshithn050 <rakshith14022001@gmail.com>
2024-07-05 12:53:55 +0530

    Like comment api created

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -3,11 +3,13 @@ import { verifyToken } from "../utils/authVerify.js";
 import {
   createNewComment,
   getPostComments,
+  likeComment,
 } from "../controllers/comment.controller.js";
 
 const router = express.Router();
 
 router.post("/createComment", verifyToken, createNewComment);
 router.get("/getPostComments/:postID", getPostComments);
+router.put("/likeComment/:postID", verifyToken, likeComment);
 
 export default router;
